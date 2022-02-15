cd6ee2a71280b8efe5615514ad2e836ee821b973
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-02-14 18:04:40 -0600

    First Commit

---
+++ challengeGenerics.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//1.-crear un seller parecido al console seller, pero que solo maneje telefonos celulares y otro que solo maneje computadoras
+
+//2.-crear una cola o queque por medio de genericos
+
+//3.- Pensar o investigar en algun otro tipo de genericos y demostrarlo en codigo
+
