6fa4226ee7fc8cdaa1c47220890f19dca09136d7
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-03-14 21:34:12 -0600

    add decorator example

--- Decorator.playground/Contents.swift
+++ Decorator.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+protocol ScoreAccesory {
+    func string(for score : Int)->String
+}
+//La concretacion del ScoreAccesory
+struct TitleAccesory: ScoreAccesory {
+    func string(for score: Int) -> String {
+        return "Score: "
+    }
+    
+}
+
+//otro tipo de concretacion de un decorator ScoreAccesory
+
+struct PointAccesoty: ScoreAccesory {
+    func string(for score: Int) -> String {
+        if score == 1 {
+            return "Point"
+        }else {
+            return "Points"
+        }
+    }
+}
+
+struct ScoreLabel {
+    var prefix: ScoreAccesory?
+    var suffix: ScoreAccesory?
+    
+    func printScore(score : Int){
+        let start = prefix?.string(for: score) ?? ""
+        let end = suffix?.string(for: score) ?? ""
+        print("\(start) \(score) \(end)")
+    }
+}
+
+var scoreLabel = ScoreLabel(prefix: TitleAccesory(), suffix: PointAccesoty())
+scoreLabel.printScore(score: 2)
+    
